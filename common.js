5e2e7a36e655ec2f9240c3373984a4d74391c263
steve-vincent <sav74sac@hotmail.com>
2019-01-12 23:56:29 -0800

    Update common.js

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-// INSERT YOUR "Initialize Firebase" CODE
+// INSERT "Initialize Firebase" SNIPPET ('var config" ... "firebase.initializeApp(config);"
 //========================================
   var config = {
     apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
@@ -12,118 +12,123 @@
 //========================================
 
 
+function debug(lbl) {
+  console.log( lbl + ": " );
+};
 
-function tdebug(str){
-  $('#tdebug').val(str + '\n' + $('#tdebug').val());
-}
+function Config(local, remote, remember) {
 
-//LocalStorage keys
-var local_pwd = "pwd";
-var local_data = "jsonstr";
-var remote_data = "encdata";
+  this.tjson;
+  this.tdata;
+  this.pwd;
+  this.expanded = new Set();
 
+  // STORAGE KEYS
+  this.LOCAL = local ? local : "bookmarks"; 
+  this.REMOTE = remote ? remote : "encdata";
+  this.REMEMBER = (remember=="false") ? false : true; //remember password?
 
-//firebase get,set
-var dbref = firebase.database().ref().child(remote_data);
-//get data
-function getData(func){ //get remote, decrypt, save local, (optional: function)
-  dbref.once("value").then( snapshot => saveLocalThen(decrypt(snapshot.val()), func) ); 
-  tdebug('loading and decrypting (remote)'); 
-}
-//save data
-function saveData(func){ //get local, encrypt, save remote, (optional: function)
-  dbref.set(encrypt(getLocal()), func);
-  tdebug('encrypting and saving (remote)'); 
-}
+  // LOCAL DATA
+  this.storeLocal = function(str) { if (this.REMEMBER) localStorage.setItem(this.LOCAL, str); return true; };
+  this.backupLocal = function() { if (this.REMEMBER) localStorage.setItem(this.LOCAL+'BAK', this.tjson); return true; };
 
+  // SAVING
+  this.save = function(data) {
+    this.tdata = data;
+    this.tjson = JSON.stringify(data);
+    return this.storeLocal(this.tjson);
+  };
+  this.saveStr = function(json) {
+    this.tjson = json;
+    this.tdata = JSON.parse(json);
+    return this.storeLocal(this.tjson);
+  };
+  this.saveExpanded = function(){ localStorage.setItem(G.LOCAL+"-expanded", JSON.stringify(Array.from(this.expanded)));}
 
-//get local
-function getLocal(){
-  return localStorage.getItem(local_data);
-}
-//save local, function
-function saveLocalThen(data, func){
-  saveLocal(data);
-  func();
-}
-//save local
-function saveLocal(data){
-  localStorage.setItem(local_data, data);
-}
-
-
-//decrypt, encrypt
-function encrypt(data){
-  return CryptoJS.AES.encrypt(data, getpwd()).toString();
-}
-function decrypt(data){
-  try{
-    return CryptoJS.AES.decrypt(data, getpwd()).toString(CryptoJS.enc.Utf8);
+  // LOAD LOCAL
+  this.saveStr(localStorage.getItem(this.LOCAL));
+  if (localStorage.getItem(this.LOCAL+"-expanded")){ 
+    this.expanded = new Set(JSON.parse(localStorage.getItem(this.LOCAL+"-expanded")));
   }
-  catch(err){
-    localStorage.removeItem(local_pwd);
-    return null; //bad password?
-  }
-}
-function getpwd(){
-  var pwd = localStorage.getItem(local_pwd);
-  if (!pwd){
-    pwd = prompt('Encryption Password');
-    localStorage.setItem(local_pwd, pwd);
-  }
-  return pwd;
-}
 
+  // REMOTE DATA (async)
+  this.dbref = firebase.database().ref().child(this.REMOTE);
+  this.fetchCallback = function(json, func) {
+    this.saveStr(json); 
+    if (func) func();
+  };
+  this.fetchData = function(func) { 
+    this.dbref.once("value").then( 
+      snapshot => this.fetchCallback(this.decrypt(snapshot.val()), func) //fetch->decrypt
+    ); 
+  };
+  this.postData = function(func) { 
+    this.dbref.set(this.encrypt(this.tjson), func); //encrypt->post
+  };
+
+  // PASSWORDS, ENCRYPT/DECRYPT
+  this.getPassword = function() { 
+    if (this.pwd) return this.pwd;
+    this.pwd = localStorage.getItem(this.LOCAL+"-pwd");
+    if (!this.pwd){
+      this.pwd = prompt('Encryption Password');
+      if (this.REMEMBER) localStorage.setItem(this.LOCAL+"-pwd", this.pwd);
+    }
+    return this.pwd;
+  };
+  this.encrypt = function(str) { return CryptoJS.AES.encrypt(str, this.getPassword()).toString() };
+  this.decrypt = function(str) {
+    try{
+      return CryptoJS.AES.decrypt(str, this.getPassword()).toString(CryptoJS.enc.Utf8);
+    }
+    catch(err){ 
+      console.log("BAD PASSWORD?");
+      this.pwd = null; //forget
+      localStorage.removeItem(this.LOCAL+"-pwd");
+      return null;
+    };
+  };
+}
 
 //add item
 function addItem(url){
-  tdebug('adding ' + url); 
   if (url){
-    var newdata = JSON.parse(getLocal());
+    var newdata = G.tdata;
     if (!newdata) newdata = [];
     newdata.push({'href':url, 'text':url});
-    saveLocal(JSON.stringify(newdata));
+    G.save(newdata);
+    return true;
   }
 }
 
-//delete or move item
+//delete, cut, paste
 function cutItem(name, moveto){
-  tdebug('cutting ' + name); 
-  var cutobj = traverse(JSON.parse(getLocal()), name, null);
-  if (moveto){ //move
-    tdebug('pasting to ' + moveto); 
-    var pasteobj =  traverse(cutobj.obj, moveto, cutobj.node);
-    if (!pasteobj.node){ //no target matched
-      pasteobj.obj.push({"nodes": [cutobj.node], "text": moveto}); //new folder
+  var cutobj = jsonQ(G.tdata);
+  //cut
+  var cutpath = cutobj.find("text", function (){return this==name}).path();
+  cutpath.pop();
+  var elx = cutobj.pathValue(cutpath); //cut for paste
+  //delete
+  var cutidx = cutpath.pop();
+  var cuttree = cutobj.pathValue(cutpath);
+  cuttree.splice(cutidx,1); 
+  cutobj.setPathValue(cutpath, cuttree);
+  //paste
+  if (moveto){ 
+    var pobj = jsonQ(G.tdata);
+    try {
+      var pastepath = pobj.find("text", function (){return this==moveto}).path();
+      //error, else existing folder...
+      pastepath.pop();
+      var elp = pobj.pathValue(pastepath); //folder
+      elp.nodes.push(elx); //append
+      pobj.setPathValue(pastepath, elp); 
     }
-    cutobj = pasteobj; //sync
+    catch(err){ //new folder
+      G.tdata.push({"text": moveto, "nodes": [elx]});
+    }
   }
-  saveLocal(JSON.stringify(cutobj.obj));
+  G.save(G.tdata);
+  return true;
 }
 
-//Traverse json
-function traverse(o, name, paste){ //node to move
-  var node;
-  jQuery(o).each(function (index){ //loop
-    if (o[index].nodes) {
-      obj = traverse(o[index].nodes, name, paste); //deeper
-      o[index].nodes = obj.obj;
-      if (obj.node) node = obj.node;
-    }
-    if (paste){ //move
-      if(o[index].text == name){ 
-        node = o[index];
-        o[index].nodes.push(paste); //paste
-        return false;
-      }
-    }
-    else{ 
-      if(o[index].text == name){ 
-        node = o[index]; //cut
-        o.splice(index,1);
-        return false;
-      }
-    }
-  });
-  return {"obj":o, "node":node};
-}
